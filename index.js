b15417345acbfe9487336076d4dde2f7731afbb7
tevpartridge <tev.partridge@gmail.com>
2021-11-13 15:40:19 +0000

    fix

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 window.addEventListener("scroll", reveal);
 
 function reveal(){
-    let reveals = document.querySelectorAll('.reveal');
+    let reveals = document.querySelectorAll(".reveal");
+    let windowHeight = window.innerHeight;
+    let revealpoint = 250;
 
-    for(var i = 0; i < reveals.length; i++){
-
-        let windowHeight = window.innerHeight;
+    for(i = 0; i < reveals.length; i++){
         let revealTop = reveals[i].getBoundingClientRect().top;
-        let revealpoint = 250;
-
         if(revealTop < windowHeight - revealpoint){
             reveals[i].classList.add('active');
         }
     }
-
 }
